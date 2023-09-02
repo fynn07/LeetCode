4422ed9381de9feb69794e4b60883e958adf9807
Mars_Ascends <pewdiestuff@gmail.com>
2023-09-02 12:53:15 +0800

    Time: 4 ms (26.98%), Space: 8.8 MB (53.13%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == val)
+            {
+               for (int j = i; j < nums.size()-1; j++)
+               {
+                   nums[j] = nums[j+1];
+               }
+               nums.pop_back();
+               i--;
+            }
+        }
+        return nums.size();
+    }
+};
