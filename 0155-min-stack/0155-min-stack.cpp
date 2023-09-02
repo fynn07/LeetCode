c8033d7697826a01782c6434e8f77dc2a10bfca7
Mars_Ascends <pewdiestuff@gmail.com>
2023-09-02 16:38:05 +0800

    Time: 22 ms (51.72%), Space: 16.2 MB (91.97%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,16 +1,26 @@
+#include <limits.h>
 class MinStack {
     vector<int> stack;
+    vector<int> minimum;
 public:
     MinStack() {
         stack;
+        minimum = {INT_MAX};
     }
     
     void push(int val) {
         stack.push_back(val);
+        if (val <= minimum.back()){
+          minimum.push_back(val);
+        }
     }
     
     void pop() {
+        if (stack.back() == minimum.back()){
+          minimum.pop_back();
+        }
         stack.pop_back();
+
     }
     
     int top() {
@@ -19,7 +29,7 @@ public:
     }
     
     int getMin() {
-        int i = *min_element(stack.begin(), stack.end());
+        int i = minimum.back();
         return i;
     }
 };
