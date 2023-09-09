3022d4210349db848f86f8e422ab73c0c87a084d
Mars_Ascends <pewdiestuff@gmail.com>
2023-09-09 23:28:13 +0800

    Time: 9 ms (72.58%), Space: 15.5 MB (75.23%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int p1 = 0;
+        int p2 = numbers.size() - 1;
+        
+        while(p1 != p2)
+        {
+            int sum = numbers[p1] + numbers[p2];
+            if(sum == target)
+            {
+                ans.push_back(p1+1);
+                ans.push_back(p2+1);
+                return ans;
+            }
+            else if (sum > target)
+            {
+                p2--;
+            }
+            else
+            {
+                p1++;
+            }
+        }
+        
+        return ans;
+    }
+};
