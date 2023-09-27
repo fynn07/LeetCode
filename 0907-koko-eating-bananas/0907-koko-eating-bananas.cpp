a216f040b7fcbd4621061f9b1be0401b7ef2a938
Mars_Ascends <pewdiestuff@gmail.com>
2023-09-27 14:17:22 +0800

    Time: 51 ms (8.25%), Space: 19.2 MB (21.54%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        sort(piles.begin(), piles.end());
+
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int minimum = right;
+
+        while(left <= right){
+            long long count = 0;
+            int mid = (left + right) / 2;
+
+            for(int i = 0; i < piles.size(); i++){
+                int add = ceil(piles[i] / (double) mid);
+                count += add;
+            }
+
+            if(count <= h){
+                minimum = min(minimum, mid);
+                right = mid -1;
+            }
+            else{
+                left = mid + 1;
+            }
+
+        }
+        return minimum;
+    }
+};
