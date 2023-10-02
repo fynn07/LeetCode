8771428d31fa04b32b98b20880948c98c050cc79
Mars_Ascends <pewdiestuff@gmail.com>
2023-10-02 18:10:15 +0800

    Time: 12 ms (61.95%), Space: 12.5 MB (5.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> output;
-        unordered_map<int, int> hash;
+        unordered_map<int, int> map;
 
         for(int i = 0; i < nums.size(); i++){
- 
-            int temp_target = target - nums[i];
-            if(hash.count(temp_target) > 0){
-                output.push_back(hash[temp_target]);
+            int find = target - nums[i];
+            if(map.count(find)){
+                output.push_back(map[find]);
                 output.push_back(i);
             }
-            
-            hash[nums[i]] = i;
+            else{
+                map[nums[i]] = i;
+            }
         }
         return output;
     }
