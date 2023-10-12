93691ea8d78720607d30ed5f5de363b014f22945
Mars_Ascends <pewdiestuff@gmail.com>
2023-10-12 17:27:30 +0800

    Time: 7 ms (36.04%), Space: 8.6 MB (20.37%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,15 +13,12 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* curr = head;
         ListNode* prev = NULL;
-
         while(curr){
             ListNode* temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
-
         }
         return prev;
-        
     }
 };
